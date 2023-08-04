e0ad3571d09d07a83bb5dc523513ce585c0346d8
MadMaia3D <33674747+MadMaia3D@users.noreply.github.com>
2023-08-04 19:03:33 -0300

    add: GetDistanceSquared method to the Vector2 class

--- Engine/Vector2.cpp
+++ Engine/Vector2.cpp
@@ -4,3 +4,9 @@ Vector2::Vector2(int x, int y) {
 	(*this).x = x;
 	(*this).y = y;
 }
+
+int Vector2::GetDistanceSquared(Vector2 position) {
+	const int deltaX = x - position.x;
+	const int deltaY = y - position.y;
+	return deltaX * deltaX + deltaY * deltaY;
+}
--- Engine/Vector2.h
+++ Engine/Vector2.h
@@ -4,6 +4,7 @@ class Vector2 {
 public:
 	Vector2() = default;
 	Vector2(int x, int y);
+	int GetDistanceSquared(Vector2 position);
 public:
 	int x = 0;
 	int y = 0;
