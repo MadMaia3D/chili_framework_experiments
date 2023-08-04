c24e4f30d8044cc32d0421bc0bc7d6179ae94dcc
MadMaia3D <33674747+MadMaia3D@users.noreply.github.com>
2023-08-04 15:15:39 -0300

    add: graphics' methods to draw transparent pixels and opaque and transparent rectangles

--- Engine/Graphics.cpp
+++ Engine/Graphics.cpp
@@ -25,11 +25,11 @@
 #include <assert.h>
 #include <string>
 #include <array>
+#include "MathUtilities.h"
 
 // Ignore the intellisense error "cannot open source file" for .shh files.
 // They will be created during the build sequence before the preprocessor runs.
-namespace FramebufferShaders
-{
+namespace FramebufferShaders {
 #include "FramebufferPS.shh"
 #include "FramebufferVS.shh"
 }
@@ -40,9 +40,8 @@ namespace FramebufferShaders
 
 using Microsoft::WRL::ComPtr;
 
-Graphics::Graphics( HWNDKey& key )
-{
-	assert( key.hWnd != nullptr );
+Graphics::Graphics(HWNDKey& key) {
+	assert(key.hWnd != nullptr);
 
 	//////////////////////////////////////////////////////
 	// create device and swap chain/get render target view
@@ -66,9 +65,9 @@ Graphics::Graphics( HWNDKey& key )
 	createFlags |= D3D11_CREATE_DEVICE_DEBUG;
 #endif
 #endif
-	
+
 	// create device and front/back buffers
-	if( FAILED( hr = D3D11CreateDeviceAndSwapChain( 
+	if (FAILED(hr = D3D11CreateDeviceAndSwapChain(
 		nullptr,
 		D3D_DRIVER_TYPE_HARDWARE,
 		nullptr,
@@ -80,44 +79,41 @@ Graphics::Graphics( HWNDKey& key )
 		&pSwapChain,
 		&pDevice,
 		nullptr,
-		&pImmediateContext ) ) )
-	{
-		throw CHILI_GFX_EXCEPTION( hr,L"Creating device and swap chain" );
+		&pImmediateContext))) {
+		throw CHILI_GFX_EXCEPTION(hr, L"Creating device and swap chain");
 	}
 
 	// get handle to backbuffer
 	ComPtr<ID3D11Resource> pBackBuffer;
-	if( FAILED( hr = pSwapChain->GetBuffer(
+	if (FAILED(hr = pSwapChain->GetBuffer(
 		0,
-		__uuidof( ID3D11Texture2D ),
-		(LPVOID*)&pBackBuffer ) ) )
-	{
-		throw CHILI_GFX_EXCEPTION( hr,L"Getting back buffer" );
+		__uuidof(ID3D11Texture2D),
+		(LPVOID*)&pBackBuffer))) {
+		throw CHILI_GFX_EXCEPTION(hr, L"Getting back buffer");
 	}
 
 	// create a view on backbuffer that we can render to
-	if( FAILED( hr = pDevice->CreateRenderTargetView( 
+	if (FAILED(hr = pDevice->CreateRenderTargetView(
 		pBackBuffer.Get(),
 		nullptr,
-		&pRenderTargetView ) ) )
-	{
-		throw CHILI_GFX_EXCEPTION( hr,L"Creating render target view on backbuffer" );
+		&pRenderTargetView))) {
+		throw CHILI_GFX_EXCEPTION(hr, L"Creating render target view on backbuffer");
 	}
 
 
 	// set backbuffer as the render target using created view
-	pImmediateContext->OMSetRenderTargets( 1,pRenderTargetView.GetAddressOf(),nullptr );
+	pImmediateContext->OMSetRenderTargets(1, pRenderTargetView.GetAddressOf(), nullptr);
 
 
 	// set viewport dimensions
 	D3D11_VIEWPORT vp;
-	vp.Width = float( Graphics::ScreenWidth );
-	vp.Height = float( Graphics::ScreenHeight );
+	vp.Width = float(Graphics::ScreenWidth);
+	vp.Height = float(Graphics::ScreenHeight);
 	vp.MinDepth = 0.0f;
 	vp.MaxDepth = 1.0f;
 	vp.TopLeftX = 0.0f;
 	vp.TopLeftY = 0.0f;
-	pImmediateContext->RSSetViewports( 1,&vp );
+	pImmediateContext->RSSetViewports(1, &vp);
 
 
 	///////////////////////////////////////
@@ -135,9 +131,8 @@ Graphics::Graphics( HWNDKey& key )
 	sysTexDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
 	sysTexDesc.MiscFlags = 0;
 	// create the texture
-	if( FAILED( hr = pDevice->CreateTexture2D( &sysTexDesc,nullptr,&pSysBufferTexture ) ) )
-	{
-		throw CHILI_GFX_EXCEPTION( hr,L"Creating sysbuffer texture" );
+	if (FAILED(hr = pDevice->CreateTexture2D(&sysTexDesc, nullptr, &pSysBufferTexture))) {
+		throw CHILI_GFX_EXCEPTION(hr, L"Creating sysbuffer texture");
 	}
 
 	D3D11_SHADER_RESOURCE_VIEW_DESC srvDesc = {};
@@ -145,38 +140,35 @@ Graphics::Graphics( HWNDKey& key )
 	srvDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
 	srvDesc.Texture2D.MipLevels = 1;
 	// create the resource view on the texture
-	if( FAILED( hr = pDevice->CreateShaderResourceView( pSysBufferTexture.Get(),
-		&srvDesc,&pSysBufferTextureView ) ) )
-	{
-		throw CHILI_GFX_EXCEPTION( hr,L"Creating view on sysBuffer texture" );
+	if (FAILED(hr = pDevice->CreateShaderResourceView(pSysBufferTexture.Get(),
+		&srvDesc, &pSysBufferTextureView))) {
+		throw CHILI_GFX_EXCEPTION(hr, L"Creating view on sysBuffer texture");
 	}
 
 
 	////////////////////////////////////////////////
 	// create pixel shader for framebuffer
 	// Ignore the intellisense error "namespace has no member"
-	if( FAILED( hr = pDevice->CreatePixelShader(
+	if (FAILED(hr = pDevice->CreatePixelShader(
 		FramebufferShaders::FramebufferPSBytecode,
-		sizeof( FramebufferShaders::FramebufferPSBytecode ),
+		sizeof(FramebufferShaders::FramebufferPSBytecode),
 		nullptr,
-		&pPixelShader ) ) )
-	{
-		throw CHILI_GFX_EXCEPTION( hr,L"Creating pixel shader" );
+		&pPixelShader))) {
+		throw CHILI_GFX_EXCEPTION(hr, L"Creating pixel shader");
 	}
-	
+
 
 	/////////////////////////////////////////////////
 	// create vertex shader for framebuffer
 	// Ignore the intellisense error "namespace has no member"
-	if( FAILED( hr = pDevice->CreateVertexShader(
+	if (FAILED(hr = pDevice->CreateVertexShader(
 		FramebufferShaders::FramebufferVSBytecode,
-		sizeof( FramebufferShaders::FramebufferVSBytecode ),
+		sizeof(FramebufferShaders::FramebufferVSBytecode),
 		nullptr,
-		&pVertexShader ) ) )
-	{
-		throw CHILI_GFX_EXCEPTION( hr,L"Creating vertex shader" );
+		&pVertexShader))) {
+		throw CHILI_GFX_EXCEPTION(hr, L"Creating vertex shader");
 	}
-	
+
 
 	//////////////////////////////////////////////////////////////
 	// create and fill vertex buffer with quad for rendering frame
@@ -191,17 +183,16 @@ Graphics::Graphics( HWNDKey& key )
 	};
 	D3D11_BUFFER_DESC bd = {};
 	bd.Usage = D3D11_USAGE_DEFAULT;
-	bd.ByteWidth = sizeof( FSQVertex ) * 6;
+	bd.ByteWidth = sizeof(FSQVertex) * 6;
 	bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
 	bd.CPUAccessFlags = 0u;
 	D3D11_SUBRESOURCE_DATA initData = {};
 	initData.pSysMem = vertices;
-	if( FAILED( hr = pDevice->CreateBuffer( &bd,&initData,&pVertexBuffer ) ) )
-	{
-		throw CHILI_GFX_EXCEPTION( hr,L"Creating vertex buffer" );
+	if (FAILED(hr = pDevice->CreateBuffer(&bd, &initData, &pVertexBuffer))) {
+		throw CHILI_GFX_EXCEPTION(hr, L"Creating vertex buffer");
 	}
 
-	
+
 	//////////////////////////////////////////
 	// create input layout for fullscreen quad
 	const D3D11_INPUT_ELEMENT_DESC ied[] =
@@ -211,12 +202,11 @@ Graphics::Graphics( HWNDKey& key )
 	};
 
 	// Ignore the intellisense error "namespace has no member"
-	if( FAILED( hr = pDevice->CreateInputLayout( ied,2,
+	if (FAILED(hr = pDevice->CreateInputLayout(ied, 2,
 		FramebufferShaders::FramebufferVSBytecode,
-		sizeof( FramebufferShaders::FramebufferVSBytecode ),
-		&pInputLayout ) ) )
-	{
-		throw CHILI_GFX_EXCEPTION( hr,L"Creating input layout" );
+		sizeof(FramebufferShaders::FramebufferVSBytecode),
+		&pInputLayout))) {
+		throw CHILI_GFX_EXCEPTION(hr, L"Creating input layout");
 	}
 
 
@@ -230,131 +220,166 @@ Graphics::Graphics( HWNDKey& key )
 	sampDesc.ComparisonFunc = D3D11_COMPARISON_NEVER;
 	sampDesc.MinLOD = 0;
 	sampDesc.MaxLOD = D3D11_FLOAT32_MAX;
-	if( FAILED( hr = pDevice->CreateSamplerState( &sampDesc,&pSamplerState ) ) )
-	{
-		throw CHILI_GFX_EXCEPTION( hr,L"Creating sampler state" );
+	if (FAILED(hr = pDevice->CreateSamplerState(&sampDesc, &pSamplerState))) {
+		throw CHILI_GFX_EXCEPTION(hr, L"Creating sampler state");
 	}
 
 	// allocate memory for sysbuffer (16-byte aligned for faster access)
-	pSysBuffer = reinterpret_cast<Color*>( 
-		_aligned_malloc( sizeof( Color ) * Graphics::ScreenWidth * Graphics::ScreenHeight,16u ) );
+	pSysBuffer = reinterpret_cast<Color*>(
+		_aligned_malloc(sizeof(Color) * Graphics::ScreenWidth * Graphics::ScreenHeight, 16u));
 }
 
-Graphics::~Graphics()
-{
+Graphics::~Graphics() {
 	// free sysbuffer memory (aligned free)
-	if( pSysBuffer )
-	{
-		_aligned_free( pSysBuffer );
+	if (pSysBuffer) {
+		_aligned_free(pSysBuffer);
 		pSysBuffer = nullptr;
 	}
 	// clear the state of the device context before destruction
-	if( pImmediateContext ) pImmediateContext->ClearState();
+	if (pImmediateContext) pImmediateContext->ClearState();
 }
 
-void Graphics::EndFrame()
-{
+void Graphics::EndFrame() {
 	HRESULT hr;
 
 	// lock and map the adapter memory for copying over the sysbuffer
-	if( FAILED( hr = pImmediateContext->Map( pSysBufferTexture.Get(),0u,
-		D3D11_MAP_WRITE_DISCARD,0u,&mappedSysBufferTexture ) ) )
-	{
-		throw CHILI_GFX_EXCEPTION( hr,L"Mapping sysbuffer" );
+	if (FAILED(hr = pImmediateContext->Map(pSysBufferTexture.Get(), 0u,
+		D3D11_MAP_WRITE_DISCARD, 0u, &mappedSysBufferTexture))) {
+		throw CHILI_GFX_EXCEPTION(hr, L"Mapping sysbuffer");
 	}
 	// setup parameters for copy operation
-	Color* pDst = reinterpret_cast<Color*>(mappedSysBufferTexture.pData );
-	const size_t dstPitch = mappedSysBufferTexture.RowPitch / sizeof( Color );
+	Color* pDst = reinterpret_cast<Color*>(mappedSysBufferTexture.pData);
+	const size_t dstPitch = mappedSysBufferTexture.RowPitch / sizeof(Color);
 	const size_t srcPitch = Graphics::ScreenWidth;
-	const size_t rowBytes = srcPitch * sizeof( Color );
+	const size_t rowBytes = srcPitch * sizeof(Color);
 	// perform the copy line-by-line
-	for( size_t y = 0u; y < Graphics::ScreenHeight; y++ )
-	{
-		memcpy( &pDst[ y * dstPitch ],&pSysBuffer[y * srcPitch],rowBytes );
+	for (size_t y = 0u; y < Graphics::ScreenHeight; y++) {
+		memcpy(&pDst[y * dstPitch], &pSysBuffer[y * srcPitch], rowBytes);
 	}
 	// release the adapter memory
-	pImmediateContext->Unmap( pSysBufferTexture.Get(),0u );
+	pImmediateContext->Unmap(pSysBufferTexture.Get(), 0u);
 
 	// render offscreen scene texture to back buffer
-	pImmediateContext->IASetInputLayout( pInputLayout.Get() );
-	pImmediateContext->VSSetShader( pVertexShader.Get(),nullptr,0u );
-	pImmediateContext->PSSetShader( pPixelShader.Get(),nullptr,0u );
-	pImmediateContext->IASetPrimitiveTopology( D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST );
-	const UINT stride = sizeof( FSQVertex );
+	pImmediateContext->IASetInputLayout(pInputLayout.Get());
+	pImmediateContext->VSSetShader(pVertexShader.Get(), nullptr, 0u);
+	pImmediateContext->PSSetShader(pPixelShader.Get(), nullptr, 0u);
+	pImmediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	const UINT stride = sizeof(FSQVertex);
 	const UINT offset = 0u;
-	pImmediateContext->IASetVertexBuffers( 0u,1u,pVertexBuffer.GetAddressOf(),&stride,&offset );
-	pImmediateContext->PSSetShaderResources( 0u,1u,pSysBufferTextureView.GetAddressOf() );
-	pImmediateContext->PSSetSamplers( 0u,1u,pSamplerState.GetAddressOf() );
-	pImmediateContext->Draw( 6u,0u );
+	pImmediateContext->IASetVertexBuffers(0u, 1u, pVertexBuffer.GetAddressOf(), &stride, &offset);
+	pImmediateContext->PSSetShaderResources(0u, 1u, pSysBufferTextureView.GetAddressOf());
+	pImmediateContext->PSSetSamplers(0u, 1u, pSamplerState.GetAddressOf());
+	pImmediateContext->Draw(6u, 0u);
 
 	// flip back/front buffers
-	if( FAILED( hr = pSwapChain->Present( 1u,0u ) ) )
-	{
-		if( hr == DXGI_ERROR_DEVICE_REMOVED )
-		{
-			throw CHILI_GFX_EXCEPTION( pDevice->GetDeviceRemovedReason(),L"Presenting back buffer [device removed]" );
+	if (FAILED(hr = pSwapChain->Present(1u, 0u))) {
+		if (hr == DXGI_ERROR_DEVICE_REMOVED) {
+			throw CHILI_GFX_EXCEPTION(pDevice->GetDeviceRemovedReason(), L"Presenting back buffer [device removed]");
 		}
-		else
-		{
-			throw CHILI_GFX_EXCEPTION( hr,L"Presenting back buffer" );
+		else {
+			throw CHILI_GFX_EXCEPTION(hr, L"Presenting back buffer");
 		}
 	}
 }
 
-void Graphics::BeginFrame()
-{
+void Graphics::BeginFrame() {
 	// clear the sysbuffer
-	memset( pSysBuffer,0u,sizeof( Color ) * Graphics::ScreenHeight * Graphics::ScreenWidth );
+	memset(pSysBuffer, 0u, sizeof(Color) * Graphics::ScreenHeight * Graphics::ScreenWidth);
 }
 
-void Graphics::PutPixel( int x,int y,Color c )
-{
-	assert( x >= 0 );
-	assert( x < int( Graphics::ScreenWidth ) );
-	assert( y >= 0 );
-	assert( y < int( Graphics::ScreenHeight ) );
+void Graphics::PutPixel(int x, int y, Color c) {
+	assert(x >= 0);
+	assert(x < int(Graphics::ScreenWidth));
+	assert(y >= 0);
+	assert(y < int(Graphics::ScreenHeight));
 	pSysBuffer[Graphics::ScreenWidth * y + x] = c;
 }
 
+void Graphics::PutPixel(int x, int y, int r, int g, int b) {
+	PutPixel(x, y, { unsigned char(r),unsigned char(g),unsigned char(b) });
+}
+
+void Graphics::PutPixel(int x, int y, Color c, float alpha) {
+	Color oldColor = GetPixel(x, y);
+
+	using namespace MathUtilities;
+	int newR = LerpInt(oldColor.GetR(), c.GetR(), alpha);
+	int newG = LerpInt(oldColor.GetG(), c.GetG(), alpha);
+	int newB = LerpInt(oldColor.GetB(), c.GetB(), alpha);
+
+	PutPixel(x, y, newR, newG, newB);
+}
+
+Color Graphics::GetPixel(int x, int y) {
+	return pSysBuffer[Graphics::ScreenWidth * y + x];
+}
+
+void Graphics::DrawRect(int x, int y, int width, int height, Color c) {
+	for (int py = 0; py < height; py++) {
+		for (int px = 0; px < width; px++) {
+			PutPixel(x + px, y + py, c);
+		}
+	}
+}
+
+void Graphics::DrawRect(Vector2 position, int width, int height, Color c) {
+	DrawRect(position.x, position.y, width, height, c);
+}
+
+void Graphics::DrawRect(RectI rect, Color c) {
+	DrawRect(rect.GetPosition(), rect.GetWidth(), rect.GetHeight(), c);
+}
+
+void Graphics::DrawRect(int x, int y, int width, int height, Color c, float alpha) {
+	for (int py = 0; py < height; py++) {
+		for (int px = 0; px < width; px++) {
+			PutPixel(x + px, y + py, c, alpha);
+		}
+	}
+}
+
+void Graphics::DrawRect(Vector2 position, int width, int height, Color c, float alpha) {
+	DrawRect(position.x, position.y, width, height, c, alpha);
+}
+
+void Graphics::DrawRect(RectI rect, Color c, float alpha) {
+	DrawRect(rect.GetPosition(), rect.GetWidth(), rect.GetHeight(), c, alpha);
+}
 
 //////////////////////////////////////////////////
 //           Graphics Exception
-Graphics::Exception::Exception( HRESULT hr,const std::wstring& note,const wchar_t* file,unsigned int line )
+Graphics::Exception::Exception(HRESULT hr, const std::wstring& note, const wchar_t* file, unsigned int line)
 	:
-	ChiliException( file,line,note ),
-	hr( hr )
-{}
+	ChiliException(file, line, note),
+	hr(hr) {
+}
 
-std::wstring Graphics::Exception::GetFullMessage() const
-{
+std::wstring Graphics::Exception::GetFullMessage() const {
 	const std::wstring empty = L"";
 	const std::wstring errorName = GetErrorName();
 	const std::wstring errorDesc = GetErrorDescription();
 	const std::wstring& note = GetNote();
 	const std::wstring location = GetLocation();
-	return    (!errorName.empty() ? std::wstring( L"Error: " ) + errorName + L"\n"
+	return    (!errorName.empty() ? std::wstring(L"Error: ") + errorName + L"\n"
 		: empty)
-		+ (!errorDesc.empty() ? std::wstring( L"Description: " ) + errorDesc + L"\n"
+		+ (!errorDesc.empty() ? std::wstring(L"Description: ") + errorDesc + L"\n"
 			: empty)
-		+ (!note.empty() ? std::wstring( L"Note: " ) + note + L"\n"
+		+ (!note.empty() ? std::wstring(L"Note: ") + note + L"\n"
 			: empty)
-		+ (!location.empty() ? std::wstring( L"Location: " ) + location
+		+ (!location.empty() ? std::wstring(L"Location: ") + location
 			: empty);
 }
 
-std::wstring Graphics::Exception::GetErrorName() const
-{
-	return DXGetErrorString( hr );
+std::wstring Graphics::Exception::GetErrorName() const {
+	return DXGetErrorString(hr);
 }
 
-std::wstring Graphics::Exception::GetErrorDescription() const
-{
-	std::array<wchar_t,512> wideDescription;
-	DXGetErrorDescription( hr,wideDescription.data(),wideDescription.size() );
+std::wstring Graphics::Exception::GetErrorDescription() const {
+	std::array<wchar_t, 512> wideDescription;
+	DXGetErrorDescription(hr, wideDescription.data(), wideDescription.size());
 	return wideDescription.data();
 }
 
-std::wstring Graphics::Exception::GetExceptionType() const
-{
+std::wstring Graphics::Exception::GetExceptionType() const {
 	return L"Chili Graphics Exception";
 }
--- Engine/Graphics.h
+++ Engine/Graphics.h
@@ -24,6 +24,8 @@
 #include <wrl.h>
 #include "ChiliException.h"
 #include "Colors.h"
+#include "Vector2.h"
+#include "RectI.h"
 
 class Graphics
 {
@@ -52,11 +54,16 @@ public:
 	Graphics& operator=( const Graphics& ) = delete;
 	void EndFrame();
 	void BeginFrame();
-	void PutPixel( int x,int y,int r,int g,int b )
-	{
-		PutPixel( x,y,{ unsigned char( r ),unsigned char( g ),unsigned char( b ) } );
-	}
 	void PutPixel( int x,int y,Color c );
+	void PutPixel(int x, int y, int r, int g, int b);
+	void PutPixel(int x, int y, Color c, float alpha);
+	Color GetPixel(int x, int y);
+	void DrawRect(int x, int y, int width, int height, Color c);
+	void DrawRect(Vector2 position, int width, int height, Color c);
+	void DrawRect(RectI rect, Color c);
+	void DrawRect(int x, int y, int width, int height, Color c, float alpha);
+	void DrawRect(Vector2 position, int width, int height, Color c, float alpha);
+	void DrawRect(RectI rect, Color c, float alpha);
 	~Graphics();
 private:
 	Microsoft::WRL::ComPtr<IDXGISwapChain>				pSwapChain;
