a0791dae1edeab720eec6b3e5ecf54248113b739
MadMaia3D <33674747+MadMaia3D@users.noreply.github.com>
2023-08-26 12:53:36 -0300

    add: LerpColor function to MathUtilities

--- Engine/MathUtilities.h
+++ Engine/MathUtilities.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Colors.h"
 
 class MathUtilities {
 public:
@@ -6,6 +7,13 @@ public:
 
 	static int LerpInt (const int a, const int b, const float t) {
 		return int(a * (1 - t) + b * (t));
+	}
+
+	static Color LerpColor(const Color& a, const Color& b, const float t) {
+		const int red = LerpInt(a.GetR(), b.GetR(), t);
+		const int green = LerpInt(a.GetG(), b.GetG(), t);
+		const int blue = LerpInt(a.GetB(), b.GetB(), t);
+		return Color(red, green, blue);
 	}
 
 	static float DegreesToRadians(float degree) {
