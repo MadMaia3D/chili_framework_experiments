5c47d9611e4059c3c35a8fe590970536c6df0739
MadMaia3D <33674747+MadMaia3D@users.noreply.github.com>
2023-08-28 22:45:34 -0300

    change: refactor Surface image loading

--- Engine/Surface.cpp
+++ Engine/Surface.cpp
@@ -6,8 +6,7 @@
 Surface::Surface(int width, int height)
 	:
 	width(width),
-	height(height)
-{
+	height(height) {
 	assert(width > 0);
 	assert(height > 0);
 	const int totalPixelsCount = width * height;
@@ -27,53 +26,47 @@ Surface::Surface(std::string file) {
 	assert(bitmapInfoHeader.biBitCount == 24 || bitmapInfoHeader.biBitCount == 32);
 	assert(bitmapInfoHeader.biCompression == BI_RGB);
 
+	const bool is32bit = bitmapInfoHeader.biBitCount == 32;
+
 	width = bitmapInfoHeader.biWidth;
 	height = bitmapInfoHeader.biHeight;
 
 	input.seekg(bitmapFileHeader.bfOffBits);
 	const int padding = (4 - ((width * 3) % 4)) % 4;
 
-	bool isInverted = false;
+	int startY = height - 1;
+	int endY = -1;
+	int yIncrement = -1;
+
 	if (height < 0) {
 		height = -height;
-		isInverted = true;
+		startY = 0;
+		endY = height;
+		yIncrement = 1;
 	}
 
 	pPixels = new Color[width * height];
 
-	if (!isInverted) {
-		for (int y = height - 1; y >= 0; y--) {
-			for (int x = 0; x < int(width); x++) {
-				unsigned char b = input.get();
-				unsigned char g = input.get();
-				unsigned char r = input.get();
-				if (bitmapInfoHeader.biBitCount == 32) {
-					unsigned char a = input.get();
-				}
-				SetPixel(x, y, Color(r, g, b));
+	for (int y = startY; y != endY; y+= yIncrement) {
+		for (int x = 0; x < int(width); x++) {
+			unsigned char b = input.get();
+			unsigned char g = input.get();
+			unsigned char r = input.get();
+			SetPixel(x, y, Color(r, g, b));
+			if (is32bit) {
+				input.seekg(1, std::ios_base::cur);
 			}
-			input.seekg(padding, std::ios_base::cur);
 		}
-	} else {
-		for (int y = 0; y < height; y++) {
-			for (int x = 0; x < int(width); x++) {
-				unsigned char b = input.get();
-				unsigned char g = input.get();
-				unsigned char r = input.get();
-				if (bitmapInfoHeader.biBitCount == 32) {
-					unsigned char a = input.get();
-				}
-				SetPixel(x, y, Color(r, g, b));
-			}
+		if(!is32bit){
 			input.seekg(padding, std::ios_base::cur);
 		}
 	}
+
 }
 
 Surface::Surface(const Surface& source)
 	:
-	Surface(source.width, source.height)
-{
+	Surface(source.width, source.height) {
 	const int totalPixelsCount = width * height;
 	for (int i = 0; i < totalPixelsCount; i++) {
 		pPixels[i] = source.pPixels[i];
@@ -84,9 +77,9 @@ Surface& Surface::operator=(const Surface& source) {
 	if (this == &source) {
 		return *this;
 	}
-	delete [] pPixels;
+	delete[] pPixels;
 	pPixels = nullptr;
-	
+
 	width = source.width;
 	height = source.height;
 
