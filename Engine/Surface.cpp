8cfe993f86d805b6fb84d1ab62269a9a9f1e3554
MadMaia3D <33674747+MadMaia3D@users.noreply.github.com>
2023-09-02 15:32:17 -0300

    implement: basic bitmap loading with support for non reversed 24 bit images for Surface class

--- Engine/Surface.cpp
+++ Engine/Surface.cpp
@@ -1,5 +1,39 @@
 #include "Surface.h"
+#include "ChiliWin.h"
 #include <cassert>
+
+Surface::Surface(std::string pathName) {
+	std::ifstream inputFile(pathName, std::ios_base::binary);
+	assert(inputFile);
+
+	BITMAPFILEHEADER bitmapFileHeader;
+	BITMAPINFOHEADER bitmapInfoHeader;
+	
+	inputFile.read(reinterpret_cast<char*>(&bitmapFileHeader), sizeof(bitmapFileHeader));
+	inputFile.read(reinterpret_cast<char*>(&bitmapInfoHeader), sizeof(bitmapInfoHeader));
+	
+	assert(bitmapFileHeader.bfType == 'MB');
+	assert(bitmapInfoHeader.biCompression == BI_RGB);
+	assert(bitmapInfoHeader.biBitCount == 24);
+
+	width = bitmapInfoHeader.biWidth;
+	height = bitmapInfoHeader.biHeight;
+	pPixels = new Color[width * height];
+	
+	inputFile.seekg(bitmapFileHeader.bfOffBits, std::ios_base::beg);
+
+	const int bmpPadding = (4 - (width % 4)) % 4 * 3;
+
+	for (int y = height - 1 ; y >= 0; y--) {
+		for (int x = 0; x < width; x++) {
+			const char b = inputFile.get();
+			const char g = inputFile.get();
+			const char r = inputFile.get();
+			pPixels[y * width + x] = Color(r, g, b);
+		}
+		inputFile.seekg(bmpPadding, std::ios_base::cur);
+	}
+}
 
 Surface::Surface(int width, int height)
 	:
--- Engine/Surface.h
+++ Engine/Surface.h
@@ -1,8 +1,11 @@
 #pragma once
+#include <string>
+#include <fstream>
 #include "Colors.h"
 
 class Surface {
 public:
+	Surface(std::string pathName);
 	Surface(int width, int height);
 	Surface(const Surface& source);
 	Surface& operator=(const Surface& source);
